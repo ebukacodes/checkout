1589ebb6af9eed1748fb98c64e9fa6c45874a6d2
ebukaakara <the1stnigerian@gmail.com>
2021-01-27 15:58:03 +0100

    first commit

--- js/switcher.js
+++ js/switcher.js
@@ -1,13 +1,27 @@
+let btn1 = document.getElementById("formButton1");
+let btn = document.getElementById("formButton");
+let bank = document.getElementById("form2");
+let card = document.getElementById("form0");
+let but1 = document.getElementById("but1");
+
+btn1.addEventListener('click', () => {
+  if (bank.style.display === "none") {
+    bank.style.display = "block";
+    card.style.display = "none";
+    but1.style.display = "none";
+  } else {
+    bank.style.display = "none";
+  }
+})
+
+// btn.addEventListener('click', () => {
  
-      $('#formButton').click(function () {
-        $('#form2').hide();
-        $('#form0').toggle();
-        $('#but1').toggle();
-      });
- 
-      $('#formButton1').click(function () {
-        $('#form0').hide();
-        $('#form2').toggle();
-        $('#but1').hide();
-      });
+//     bank.style.display = "none";
+//     card.style.display = "block";
+//     but1.style.display = "block";
+  
+// })
+
+
+btn.addEventListener('click', () => window.location.reload());
   
