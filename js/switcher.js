a9d1e45b37e4a58943f0724e9a297679427da2b1
ebukaakara <the1stnigerian@gmail.com>
2021-01-27 15:04:46 +0100

    first commit

---
+++ js/switcher.js
@@ -0,0 +1,13 @@
+ 
+      $('#formButton').click(function () {
+        $('#form2').hide();
+        $('#form0').toggle();
+        $('#but1').toggle();
+      });
+ 
+      $('#formButton1').click(function () {
+        $('#form0').hide();
+        $('#form2').toggle();
+        $('#but1').hide();
+      });
+  
